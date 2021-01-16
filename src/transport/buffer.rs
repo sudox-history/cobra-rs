310fe2da93684b540867f50eaadb44bdd892cff4
undefined7887 <undefined7887@gmail.com>
2021-01-16 23:51:41 +0300

    [BACKUP]

--- src/transport/buffer.rs
+++ src/transport/buffer.rs
@@ -1,21 +1,99 @@
+use crate::transport::frame::{Frame, PartFrame};
+
+//...[start_pointer...end_pointer)...
 pub struct FrameBuffer {
     content: Vec<u8>,
-    front_pointer: usize,
-    max_read_size: usize,
+    start_pointer: usize,
+    end_pointer: usize,
+    header_size: usize,
 }
 
 impl FrameBuffer {
-    pub fn new(buffer_size: usize, max_read_size: usize) -> FrameBuffer {
+    pub fn new(buffer_size: usize, header_size: usize) -> FrameBuffer {
         FrameBuffer {
-           content: vec![0; buffer_size],
-           front_pointer: 0, 
-           max_read_size,
+            content: vec![0; buffer_size],
+            start_pointer: 0,
+            end_pointer: 0,
+            header_size,
         }
     }
 
     pub fn get_slice_to_read(&mut self) -> &mut [u8] {
-        self[front_pointer..(front_pointer + )]
+        &mut self.content[self.end_pointer..]
     }
-    
-    
+
+    pub fn readed_bytes(&mut self, n: usize) {
+        self.end_pointer += n;
+    }
+
+    fn current_byte(&mut self) -> u8 {
+        self.start_pointer += 1;
+        self.content[self.start_pointer - 1]
+    }
+
+    fn get_header(&mut self) -> Option<usize> {
+        if self.end_pointer - self.start_pointer >= self.header_size {
+            let mut result: usize = 0;
+
+            // Intel (x86) -> LE
+            // ARM -> BE
+
+            for i in 0..self.header_size {
+                result <<= 8;
+                result += self.current_byte() as usize;
+            }
+
+            Some(result)
+        } else {
+            None
+        }
+    }
+
+    fn make_backup(&self) -> (usize, usize) {
+        (self.start_pointer, self.end_pointer)
+    }
+
+    fn use_backup(&mut self, backup: (usize, usize)) {
+        self.start_pointer = backup.0;
+        self.end_pointer = backup.1;
+    }
+
+    pub fn try_get_frame(&mut self) -> Option<Frame> {
+        let backup = self.make_backup();
+        let header = self.get_header()?;
+
+        if self.end_pointer - self.start_pointer >= header {
+            let mut result = Frame::default();
+
+            let start = self.start_pointer;
+            let end = start + header + 1;
+
+            self.start_pointer += header;
+
+            Some(Frame::new(self.connect[start..end]))
+        } else {
+            self.use_backup(backup);
+            None
+        }
+    }
+
+    pub fn empty(&self) -> bool {
+        self.start_pointer < self.end_pointer
+    }
+
+    pub fn try_get_part_frame(&mut self) -> Option<PartFrame> {
+        let backup = self.make_backup();
+        let header = self.get_header()?;
+
+        if !self.empty() && self.end_pointer - self.start_pointer < header {
+
+            let start = self.start_pointer;
+            let end = self.end_pointer;
+
+            Some(PartFrame::new(self.content[start..end]))
+        } else {
+            self.use_backup(backup);
+            None
+        }
+    }
 }
--- src/transport/connection.rs
+++ src/transport/connection.rs
@@ -1,62 +1,79 @@
-use std::{
-    io,
-    sync::Arc,
-};
+use std::{io, net::Shutdown, sync::Arc};
 
 use tokio::{
-    net::{ToSocketAddrs, TcpStream},
-    sync::mpsc,
+    net::{TcpStream, ToSocketAddrs},
+    sync::{broadcast, oneshot},
 };
 
 use crate::transport::buffer::FrameBuffer;
+use crate::transport::frame::Frame;
 
+use super::frame;
 
 pub struct Connection {
     shared_state: Arc<SharedState>,
+    receiver: broadcast::Receiver<Frame>,
+    close_sender: oneshot::Sender<bool>, 
 }
 
 struct SharedState {
     tcp_stream: TcpStream,
-    sender: tokio::sync::mpsc::Sender<FrameBuffer>,
+    sender: broadcast::Sender<Frame>,
+    close_receiver: oneshot::Receiver<bool>
 }
 
 impl Connection {
     pub async fn connect<T: ToSocketAddrs>(addr: T) -> io::Result<Connection> {
-        let (sender, mut receiver) = mpsc::channel(100);
-        let shared_state = Arc::new(
-            SharedState {
-                tcp_stream: TcpStream::connect(addr).await?,
-                send
-            }
-        );
-
+        let (sender, mut receiver) = broadcast::channel(100);
+        let (close_sender, mut close_receiver) = oneshot::channel();
+        let shared_state = Arc::new(SharedState {
+            tcp_stream: TcpStream::connect(addr).await?,
+            sender,
+            close_receiver,
+        });
 
         // Cool code by bulat <3
-        tokio::spawn(
-            Connection::read_loop(shared_state.clone())
-        );
+        tokio::spawn(Connection::read_loop(shared_state.clone()));
 
-        Ok(
-            Connection {
-                shared_state
-            }
-        )
+        Ok(Connection {
+            shared_state,
+            receiver,
+            close_sender,
+        })
     }
 
-    fn close(&self) -> io::Result<()> {
-        self.close()
+    async fn read(&mut self) -> io::Result<Frame> {
+        match self.receiver.recv().await {
+            Ok(frame) => frame,
+            Err(_) => Err(io::Error::from(io::ErrorKind::NotConnected)),
+        }
+    }
+
+    fn write(&self, frame: Frame) {}
+
+    fn drop(self) {
+        self.close_sender.send(true).unwrap();
     }
     
     async fn read_loop(shared_state: Arc<SharedState>) -> io::Result<()> {
-        let mut buffer = FrameBuffer::new();
+        let mut buffer = FrameBuffer::new(100000, 2);
         loop {
-            shared_state.tcp_stream.readable().await?;
+            tokio::select!{
+                _ = shared_state.tcp_stream.readable() => {
 
+                }
+                _ = shared_state.close_receiver.try_recv() => {
+
+                }
+            };
+
+            // TODO: Handle queue overflow
+            // TODO: Update ping timer
 
             match shared_state.tcp_stream.try_read(buffer.get_slice_to_read()) {
                 Ok(n) => {
                     buffer.readed_bytes(n);
-                },
+                }
                 Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
                     continue;
                 }
@@ -65,12 +82,9 @@ impl Connection {
                 }
             }
 
-            while let Ok(new_frame) = buffer.try_get_frame() {
-                 shared_state.sender.send(new_frame);
+            while let Some(new_frame) = buffer.try_get_frame() {
+                shared_state.sender.send(new_frame).unwrap();
             }
-
-            todo!();
-            // TODO: Реализуем жопу
         }
     }
 }
