32f121f9fccce32f743b2cd07a0ad246acfbf047
evgeniy <kletska.epa@hotmail.com>
2021-01-28 21:34:57 +0300

    Implementation of transport::frame::Frame & transport::frame::Buffer

--- src/transport/frame.rs
+++ src/transport/frame.rs
@@ -1,11 +1,11 @@
-use bytes::BufMut;
+use bytes::{Buf, BufMut};
 
 use crate::transport::pool::Kind;
 use crate::transport::constants::{
     FRAME_LEN_BYTES, FRAME_KIND_BYTES, FRAME_HEADER_BYTES
 };
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct Frame {
     kind: u8,
     pub data: Vec<u8>,
@@ -19,12 +19,9 @@ impl Frame {
         }
     }
 
-    pub fn from_slice(slice: &[u8]) -> Self {
-        let kind = slice[FRAME_LEN_BYTES];
-
-        // Frame data
-        let data = Vec::with_capacity(slice.len()-FRAME_HEADER_BYTES);
-        data.copy_from_slice(&slice[FRAME_HEADER_BYTES..]);
+    pub fn from_slice(kind: u8, slice: &[u8]) -> Self {
+        let mut data = Vec::with_capacity(slice.len());
+        data.copy_from_slice(slice);
 
         Frame {
             kind,
@@ -60,8 +57,8 @@ impl Kind<u8> for Frame {
 // frame::Buff
 pub struct Buffer {
     content: Vec<u8>,
-    start_data: usize,
-    end_data: usize
+    start_pointer: usize,
+    end_pointer: usize,
     part_frame: Option<(usize, Frame)>
 }
 
@@ -69,82 +66,78 @@ impl Buffer {
     pub fn new(buffer_size: usize) -> Buffer {
         Buffer {
             content: vec![0; buffer_size],
-            start_data: 0,
-            end_data: 0,
+            start_pointer: 0,
+            end_pointer: 0,
             part_frame: None,
         }
     }
 
     pub fn get_slice_to_read(&mut self) -> &mut [u8] {
-        &mut self.content[self.end_data..]
+        &mut self.content[self.end_pointer..]
     }
 
     pub fn readed_bytes(&mut self, n: usize) {
-        self.end_data += n;
+        self.end_pointer += n;
     }
 
     fn current_byte(&mut self) -> u8 {
-        self.start_data += 1;
-        self.content[self.start_data - 1]
+        self.start_pointer += 1;
+        self.content[self.start_pointer - 1]
     }
 
-    fn get_header(&mut self) -> Option<(usize, usize)> {
-        if self.end_data - self.start_pointer >= FRAME_LEN_BYTES + FRAME_KIND_BYTES {
-            todo!()
+    fn get_header(&mut self) -> Option<(usize, u8)> {
+        if self.end_pointer - self.start_pointer >= FRAME_HEADER_BYTES {
+            let start = self.start_pointer;
+            let end = start + FRAME_LEN_BYTES + 1;
+            self.start_pointer += FRAME_HEADER_BYTES;
+            let mut buf = &self.content[start..end];
+            Some((buf.get_uint(FRAME_LEN_BYTES) as usize, self.content[end]))
         } else {
-            for (new_index, old_index) in (self.start_data..self.end_data).enumerate() {
+            for (new_index, old_index) in (self.start_pointer..self.end_pointer).enumerate() {
                 self.content[new_index] = self.content[old_index];     
             }
-            self.end_data = self.data_len();
-            self.start_data = 0;
+            self.end_pointer = self.data_len();
+            self.start_pointer = 0;
             None
         }
     }
 
     pub fn try_get_frame(&mut self) -> Option<Frame> {
-        if let Some((len, part_frame)) = self.part_frame.move() {
+        if let Some((len, mut part_frame)) = self.part_frame.take() {
             if len <= part_frame.len() + self.data_len() {
-                while part_frame.len() < len() {
-                    part_frame.data.push(self.current_byte())
+                while part_frame.len() < len {
+                    part_frame.data.push(self.current_byte());
                 }
+                Some(part_frame)
             } else {
                 self.part_frame = Some((len, part_frame));
                 None
             }
         } else {
-            let header = self.get_header()?;
-            if self.data_len() >= header {
-                let mut result = Frame::default();
-                let start = self.data_interval.0;
-                let end = start + header + 1;
-                self.data_interval.0 += header;
-                Some(Frame::from_slice(self.connect[start..end]))
+            let (len, kind) = self.get_header()?;
+            if self.data_len() >= len {
+                let start = self.start_pointer;
+                let end = start + len + 1;
+                let slice = &self.content[start..end];
+                self.start_pointer += len;
+                Some(Frame::from_slice(kind, slice))
             } else {
-                todo!()
+                let start = self.start_pointer;
+                let end = self.end_pointer;
+                let slice = &self.content[start..end]; 
+                self.start_pointer = 0;
+                self.end_pointer = 0;
+                self.part_frame = Some((len, Frame::from_slice(kind, slice)));
+                None
             }
         }
     }
 
     pub fn empty(&self) -> bool {
-        self.start_data == self.end_data
+        self.start_pointer == self.end_pointer
     }
 
     fn data_len(&self) -> usize {
-        self.end_data - self.start_data
-    }
-
-    pub fn try_get_part_frame(&mut self) -> Option<PartFrame> {
-        let header = self.get_header()?;
-
-        if !self.empty() && self.data_len() < header {
-
-            let start = self.data_interval.0;
-            let end = self.data_interval.1;
-
-            Some(PartFrame::new(self.content[start..end]))
-        } else {
-            self.data_interval = backup;
-            None
-        }
+        self.end_pointer - self.start_pointer
     }
 }
