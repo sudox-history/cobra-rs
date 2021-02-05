b43f436a065f5768f0611a057fe304c749a8373b
undefined7887 <undefined7887@gmail.com>
2021-02-06 00:10:33 +0300

    Implementation of transport::buffer::ConcatBuffer

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,16 @@ use std::ops::{DerefMut, Deref};
 
 #[tokio::main]
 async fn main() {
-    let mut bytes = BytesMut::with_capacity(11);
-    bytes.put_i32(10);
-    bytes.put_i32(12);
-    bytes.get_i32();
-    bytes.get_i32();
+    let mut bytes = BytesMut::with_capacity(4);
+    bytes.put_u8(123);
+    bytes.put_u8(123);
+    bytes.put_u8(123);
+    bytes.get_u8();
 
     // 00 00 00 00 | 00 00 00 12 | 00 00 00
     //bytes.copy_within(4..8, 0);
 
     // ATTENTION!!!: Moving bytes to start (capacity-len, capacity]
-    bytes.reserve(bytes.capacity());
+    bytes.reserve(bytes.capacity() - bytes.len() + 1);
     println!("{:?}", bytes.capacity());
 }
--- src/transport/buffer.rs
+++ src/transport/buffer.rs
@@ -4,6 +4,7 @@ use std::fmt::Debug;
 
 pub trait Chunk: DerefMut<Target=Vec<u8>> + Debug {
     fn header_len() -> usize;
+    fn with_capacity(capacity: usize) -> Self;
 }
 
 pub struct ConcatBuffer<T: Chunk> {
@@ -19,53 +20,47 @@ impl<T: Chunk> ConcatBuffer<T> {
         }
     }
 
-    // fn try_read_partial_chunk(&mut self) -> Option<T> {
-    //     //Note: this function must only be called if there is a chunk
-    //     let (len, mut partial_chunk) = self.partial_chunk
-    //         .take()
-    //         .expect("attempt to read a non-existent chunk");
-    //
-    //     if len <= partial_chunk.len() + self.inner.len() {
-    //         partial_chunk.extend_from_slice(&self.inner[..len - partial_chunk.len()]);
-    //         Some(partial_chunk)
-    //     } else {
-    //         self.partial_chunk = Some((len, partial_chunk));
-    //         None
-    //     }
-    // }
+    pub fn try_read_chunk(&mut self) -> Option<T> {
+        match self.partial_chunk.take() {
+            Some((len, partial_chunk)) =>
+                self.try_read_partial_chunk(len, partial_chunk),
 
-    // pub fn try_read_chunk(&mut self) -> Option<T> {
-    //     match self.partial_chunk {
-    //         Some((len, mut partial_chunk)) => {
-    //             if len <= partial_chunk.len() + self.inner.len() {
-    //                 partial_chunk.extend_from_slice(&self.inner[..len - partial_chunk.len()]);
-    //                 Some(partial_chunk)
-    //             } else {
-    //                 self.partial_chunk = Some((len, partial_chunk));
-    //                 None
-    //             }
-    //         },
-    //
-    //         None => {
-    //             let len = self.try_read_header()?;
-    //             if len <= self.inner.len() as u64 {
-    //                 Some(T::)
-    //             } else {
-    //                 let start = self.read_pointer;
-    //                 let end = self.write_pointer;
-    //                 let slice = &self.inner[start..end];
-    //                 self.read_pointer = 0;
-    //                 self.write_pointer = 0;
-    //                 self.part_frame = Some((len, Frame::from_slice(kind, slice)));
-    //                 None
-    //             }
-    //         }
-    //     }
-    // }
+            None =>
+                self.try_read_full_chunk(),
+        }
+    }
 
-    fn try_read_header(&mut self) -> Option<u64> {
+    fn try_read_partial_chunk(&mut self, len: usize, mut partial_chunk: T) -> Option<T> {
+        let partial_len = partial_chunk.len();
+
+        if len <= partial_len + self.inner.len() {
+            partial_chunk.extend_from_slice(&self.inner[..len - partial_len]);
+            Some(partial_chunk)
+        } else {
+            self.partial_chunk = Some((len, partial_chunk));
+            None
+        }
+    }
+
+    fn try_read_full_chunk(&mut self) -> Option<T> {
+        let len = self.try_read_header()?;
+        let mut chunk = T::with_capacity(len);
+
+        if len <= self.inner.len() {
+            chunk.extend_from_slice(&self.inner[..len]);
+            Some(chunk)
+        } else {
+            chunk.extend_from_slice(&self.inner);
+            self.partial_chunk = Some((len, chunk));
+
+            self.fragment();
+            None
+        }
+    }
+
+    fn try_read_header(&mut self) -> Option<usize> {
         if self.inner.len() >= T::header_len() {
-            Some(self.inner.get_uint(T::header_len()))
+            Some(self.inner.get_uint(T::header_len()) as usize)
         } else {
             self.fragment();
             None
@@ -73,10 +68,19 @@ impl<T: Chunk> ConcatBuffer<T> {
     }
 
     fn fragment(&mut self) {
-        // This action will move data to the start of the buffer
-        // or cleans it if there is no data
+        // This action will move data to the start of the buffer.
+        // If there is no data, it will also move the cursor to the start.
         // Read .reserve() documentation for more details
-        self.inner.reserve(self.inner.capacity());
+        self.inner.reserve(self.inner.capacity() - self.inner.len() + 1);
+    }
+}
+
+impl<T: Chunk> Default for ConcatBuffer<T> {
+    fn default() -> Self {
+        ConcatBuffer {
+            inner: BytesMut::with_capacity(256usize.pow(T::header_len() as u32) - 1),
+            partial_chunk: None,
+        }
     }
 }
 
