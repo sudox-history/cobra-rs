ae3a5d66c181f5f841b9c5fea6567ad1f2c14112
bularond <bularond@protonmail.com>
2021-02-06 11:44:56 +0300

    Update pool tests

--- tests/pool_tests.rs
+++ tests/pool_tests.rs
@@ -1,6 +1,25 @@
 #![allow(unused)]
 use cobra_rs::transport::pool::*;
-use cobra_rs::transport::frame::*;
+
+struct TestFrame {
+    key: u8,
+    value: i32,
+}
+
+impl TestFrame {
+    fn create(key: u8, value: i32) -> Self {
+        TestFrame {
+            key,
+            value,
+        }
+    }
+}
+
+impl Kind<u8> for TestFrame {
+    fn kind(&self) -> u8 {
+        self.key
+    }
+}
 
 #[tokio::test]
 async fn one_read_two_write() {
@@ -12,23 +31,35 @@ async fn one_read_two_write() {
     const KIND_B: u8 = 1;
 
     tokio::spawn(async move {
-        let package = Frame::from_vec(KIND_A, vec![0; 100000]);
-        write_pool_a.write(package).await;
+        let package_a = TestFrame::create(KIND_A, 0);
+        let package_b = TestFrame::create(KIND_A, 1);
+        write_pool_a.write(package_a).await;
+        write_pool_a.write(package_b).await;
     });
 
     tokio::spawn(async move {
-        let package = Frame::from_vec(KIND_B, vec![1, 2, 3, 4, 5]);
-        write_pool_b.write(package).await;
+        let package_a = TestFrame::create(KIND_B, 2);
+        let package_b = TestFrame::create(KIND_B, 3);
+        write_pool_b.write(package_a).await;
+        write_pool_b.write(package_b).await;
     });
 
     assert_eq!(read_pool.read(KIND_A)
                    .await
                    .unwrap()
-                   .data, vec![0; 100000]);
+                   .value, 0);
+    assert_eq!(read_pool.read(KIND_A)
+                   .await
+                   .unwrap()
+                   .value, 1);
     assert_eq!(read_pool.read(KIND_B)
                    .await
                    .unwrap()
-                   .data, vec![1, 2, 3, 4, 5]);
+                   .value, 2);
+    assert_eq!(read_pool.read(KIND_B)
+                   .await
+                   .unwrap()
+                   .value, 3);
 }
 
 #[tokio::test]
@@ -41,8 +72,8 @@ async fn two_read_one_write() {
     const KIND_B: u8 = 1;
 
     tokio::spawn(async move {
-        let package_a = Frame::from_vec(KIND_A, vec![0; 100000]);
-        let package_b = Frame::from_vec(KIND_B, vec![1, 2, 3, 4, 5]);
+        let package_a = TestFrame::create(KIND_A, 0);
+        let package_b = TestFrame::create(KIND_B, 1);
         write_pool.write(package_a).await;
         write_pool.write(package_b).await;
     });
@@ -50,11 +81,11 @@ async fn two_read_one_write() {
     assert_eq!(read_pool_a.read(KIND_A)
                    .await
                    .unwrap()
-                   .data, vec![0; 100000]);
+                   .value, 0);
     assert_eq!(read_pool_b.read(KIND_B)
                    .await
                    .unwrap()
-                   .data, vec![1, 2, 3, 4, 5]);
+                   .value, 1);
 }
 
 #[tokio::test]
@@ -68,23 +99,23 @@ async fn two_read_two_write() {
     const KIND_B: u8 = 1;
 
     tokio::spawn(async move {
-        let package_a = Frame::from_vec(KIND_A, vec![0; 100000]);
+        let package_a = TestFrame::create(KIND_A, 0);
         write_pool_a.write(package_a).await;
     });
 
     tokio::spawn(async move {
-        let package_b = Frame::from_vec(KIND_B, vec![1, 2, 3, 4, 5]);
+        let package_b = TestFrame::create(KIND_B, 1);
         write_pool_b.write(package_b).await;
     });
 
     assert_eq!(read_pool_a.read(KIND_A)
                    .await
                    .unwrap()
-                   .data, vec![0; 100000]);
+                   .value, 0);
     assert_eq!(read_pool_b.read(KIND_B)
                    .await
                    .unwrap()
-                   .data, vec![1, 2, 3, 4, 5]);
+                   .value, 1);
 }
 
 #[tokio::test]
@@ -93,33 +124,38 @@ async fn stress_test() {
 
     const KIND_A: u8 = 0;
 
-    for _ in 0..100000 {
+    for i in 0..100000 {
         let write_pool = read_pool.clone();
         tokio::spawn(async move {
-            let package = Frame::from_vec(KIND_A, vec![1, 2, 3]);
+            let package = TestFrame::create(KIND_A, i);
             write_pool.write(package).await;
         });
     }
 
-    for _ in 0..100000 {
+    for i in 0..100000 {
         assert_eq!(read_pool.read(KIND_A)
                        .await
                        .unwrap()
-                       .data, vec![1, 2, 3]);
+                       .value, i);
     }
 }
 
 #[tokio::test]
 #[should_panic]
 async fn write_err() {
-    let close_pool: Pool<u8, Frame> = Pool::new();
+    let close_pool: Pool<u8, TestFrame> = Pool::new();
     let write_pool = close_pool.clone();
 
     const KIND_A: u8 = 0;
 
     close_pool.close().await;
-    let package = Frame::from_vec(KIND_A, vec![1, 2, 3]);
-    write_pool.write(package).await.unwrap();
+    let package = TestFrame::create(KIND_A, 0);
+    match write_pool.write(package).await {
+        Ok(_) => {},
+        Err(_) => {
+            panic!("Write err")
+        }
+    }
 }
 
 #[tokio::test]
@@ -131,7 +167,7 @@ async fn err_after_write() {
     const KIND_A: u8 = 0;
 
     tokio::spawn(async move {
-        let package = Frame::from_vec(KIND_A, vec![1, 2, 3]);
+        let package = TestFrame::create(KIND_A, 0);
         write_pool.write(package).await;
         write_pool.close().await;
     });
@@ -139,7 +175,7 @@ async fn err_after_write() {
     assert_eq!(read_pool.read(KIND_A)
                    .await
                    .unwrap()
-                   .data, vec![1, 2, 3]);
+                   .value, 0);
     read_pool.read(KIND_A).await.unwrap();
 }
 
@@ -153,23 +189,23 @@ async fn read_any() {
     const KIND_B: u8 = 1;
 
     tokio::spawn(async move {
-        let package = Frame::from_vec(KIND_A, vec![1, 2, 3]);
+        let package = TestFrame::create(KIND_A, 0);
         write_any_pool_a.write(package).await;
     });
 
     tokio::spawn(async move {
-        let package = Frame::from_vec(KIND_B, vec![1, 2, 3]);
+        let package = TestFrame::create(KIND_B, 0);
         write_any_pool_b.write(package).await;
     });
 
     assert_eq!(read_any_pool.read_any()
                    .await
                    .unwrap()
-                   .data, vec![1, 2, 3]);
+                   .value, 0);
     assert_eq!(read_any_pool.read_any()
                    .await
                    .unwrap()
-                   .data, vec![1, 2, 3]);
+                   .value, 0);
 }
 
 #[tokio::test]
@@ -183,14 +219,14 @@ async fn data_order() {
 
     tokio::spawn(async move {
         for i in 0..5 {
-            let package = Frame::from_vec(KIND_A, vec![i]);
+            let package = TestFrame::create(KIND_A, i);
             write_pool_a.write(package).await;
         }
     });
 
     tokio::spawn(async move {
         for i in 0..5 {
-            let package = Frame::from_vec(KIND_B, vec![i]);
+            let package = TestFrame::create(KIND_B, i);
             write_pool_b.write(package).await;
         }
     });
@@ -199,10 +235,10 @@ async fn data_order() {
         assert_eq!(read_pool.read(KIND_A)
             .await
             .unwrap()
-            .data, vec![i]);
+            .value, i);
         assert_eq!(read_pool.read(KIND_B)
-                       .await
-                       .unwrap()
-                       .data, vec![i]);
+            .await
+            .unwrap()
+            .value, i);
     }
 }
