6745d665038085dbd004355e48fa3ad1e429b673
bularond <bularond@protonmail.com>
2021-02-07 22:15:53 +0300

    Update conn.rs architecture

--- src/transport/conn.rs
+++ src/transport/conn.rs
@@ -1,76 +1,32 @@
 use tokio::net::TcpStream;
 use std::io;
-use std::hash::Hash;
 use std::sync::Arc;
 
-use crate::transport::pool::{PoolAny, Pool, Kind, WriteError};
-use crate::transport::buffer::{Chunk, ConcatBuffer};
+use crate::transport::pool::{PoolAny, Pool, WriteError};
+use crate::transport::buffer::ConcatBuffer;
+use crate::transport::frame::Frame;
 
-pub struct Conn<K, F>
-where
-    K: Eq + Hash + Send + 'static,
-    F: Kind<K> + Chunk + Send + 'static
-{
-    write_pool: PoolAny<F>,
-    read_pool: Pool<K, F>
+pub struct Conn {
+    write_pool: PoolAny<Frame>,
+    read_pool: Pool<u8, Frame>
 }
 
-impl<K, F> Conn<K, F>
-    where
-    K: Eq + Hash + Send + 'static,
-    F: Kind<K> + Chunk + Send + 'static
-{
+impl Conn {
     pub async fn connect(addr: &str) -> io::Result<Self> {
         let read_tcp_stream = Arc::new(TcpStream::connect(addr).await?);
         let write_tcp_stream = read_tcp_stream.clone();
 
         let self_read_pool = Pool::new();
-        let read_pool: Pool<K, F>  = self_read_pool.clone();
+        let read_pool  = self_read_pool.clone();
 
-        let self_write_pool = PoolAny::new();
-        let write_pool: PoolAny<F> = self_write_pool.clone();
+        let self_write_pool : PoolAny<Frame> = PoolAny::new();
+        let write_pool = self_write_pool.clone();
 
-        let mut buffer: ConcatBuffer<F> = ConcatBuffer::default();
+        let buffer = ConcatBuffer::default();
 
-        // Read
-        tokio::spawn(async move {
-            loop {
-                if read_tcp_stream.readable().await.is_err() {
-                    break
-                }
+        tokio::spawn(Conn::read_loop(read_tcp_stream, read_pool, buffer));
 
-                match read_tcp_stream.try_read(&mut buffer) {
-                    Ok(0) => break,
-                    Ok(_) => {}
-                    Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue,
-                    Err(_) => break
-                }
-
-                if let Some(chunk) = buffer.try_read_chunk() {
-                    if read_pool.write(chunk).await.is_err() {
-                        break;
-                    }
-                }
-            }
-            read_pool.close().await;
-        });
-
-        // Write
-        tokio::spawn(async move {
-            while let Some(frame) = write_pool.read().await {
-                match write_tcp_stream.writable().await {
-                    Ok(_) => {}
-                    Err(_) => break
-                }
-
-                match write_tcp_stream.try_write(&frame) {
-                    Ok(_) => break,
-                    Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue,
-                    Err(_) => break
-                }
-            }
-            write_pool.close().await;
-        });
+        tokio::spawn(Conn::write_loop(write_tcp_stream, write_pool));
 
         Ok(Conn {
             write_pool: self_write_pool,
@@ -78,13 +34,58 @@ impl<K, F> Conn<K, F>
         })
     }
 
+    async fn read_loop(read_tcp_stream: Arc<TcpStream>,
+                       read_pool: Pool<u8, Frame>,
+                       mut buffer: ConcatBuffer<Frame>) {
+        loop {
+            if read_tcp_stream.readable().await.is_err() {
+                break;
+            }
+
+            match read_tcp_stream.try_read(&mut buffer) {
+                Ok(0) => break,
+                Ok(_) => {}
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue,
+                Err(_) => break
+            }
+
+            while let Some(chunk) = buffer.try_read_chunk() {
+                if read_pool.write(chunk).await.is_err() {
+                    break;
+                }
+            }
+        }
+        read_pool.close().await;
+    }
+
+    async fn write_loop(write_tcp_stream: Arc<TcpStream>,
+                        write_pool: PoolAny<Frame>) {
+        while let Some(frame) = write_pool.read().await {
+            if write_tcp_stream.writable().await.is_err() {
+                break;
+            }
+
+            match write_tcp_stream.try_write(&frame) {
+                Ok(_) => break,
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue,
+                Err(_) => break
+            }
+        }
+        write_pool.close().await;
+    }
+
     // Return None if connection close
-    pub async fn read(&self, kind: K) -> Option<F> {
+    pub async fn read(&self, kind: u8) -> Option<Frame> {
         self.read_pool.read(kind).await
     }
 
     // Return WriteError<F> if connection close
-    pub async fn write(&self, frame: F) -> Result<(), WriteError<F>> {
+    pub async fn write(&self, frame: Frame) -> Result<(), WriteError<Frame>> {
         self.write_pool.write(frame).await
+    }
+
+    pub async fn close(&self) {
+        self.write_pool.close().await;
+        self.read_pool.close().await;
     }
 }
